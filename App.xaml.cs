265b0c2dcb54ab68f9323b9b8ba34249a7baf4c1
Voltaire-000 <87976056+Voltare-000@users.noreply.github.com>
2024-02-17 18:56:48 +0700

    in process

--- App.xaml.cs
+++ App.xaml.cs
@@ -5,8 +5,9 @@
         public App()
         {
             InitializeComponent();
+            MainPage = new NavigationPage(new MainPage());
 
-            MainPage = new AppShell();
+            //MainPage = new AppShell();
         }
     }
 }
---
+++ dogs.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiEver;
+
+public partial class NewPage1 : ContentPage
+{
+	public NewPage1()
+	{
+		InitializeComponent();
+	}
+}
