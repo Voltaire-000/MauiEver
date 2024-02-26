6d560f998aa91b0e90603516f2af6c41c9fbf9f5
Voltaire-000 <87976056+Voltare-000@users.noreply.github.com>
2024-02-26 16:04:23 +0700

    add code behind to JsonView page

--- JsonView.xaml.cs
+++ JsonView.xaml.cs
@@ -5,5 +5,21 @@ public partial class JsonView : ContentPage
 	public JsonView()
 	{
 		InitializeComponent();
+
+		StackLayout stackLayout = new StackLayout();
+		stackLayout.Add(new Label { Text = "No results matched your filter." });
+		stackLayout.Add(new Label { Text = "Try a different filter." });
+
+		SearchBar searchBar = new SearchBar();
+		CollectionView collectionView = new CollectionView
+		{
+			EmptyView = new ContentView
+			{
+				Content = stackLayout
+			}
+
+		};
+		collectionView.SetBinding(ItemsView.ItemsSourceProperty, "thermoSource");
+
 	}
 }
